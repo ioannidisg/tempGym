550a534f790f22e3775ee82173afa8a3e85c6a33
ioannidisg <153500069+ioannidisg@users.noreply.github.com>
2024-11-14 14:05:15 +0200

    Add eslint.config.js for ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+// eslint.config.js
+export default [
+    {
+      files: ["**/*.js"],
+      rules: {
+        "no-unused-vars": "warn",
+        // πρόσθεσε τους κανόνες που χρειάζεσαι
+      },
+    },
+  ];
+  
